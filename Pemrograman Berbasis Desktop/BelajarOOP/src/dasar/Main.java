2551e82718ee10ee492940ede5f0342b6f7b06e7
Ryo Richie <ryorichie@gmail.com>
2023-12-11 09:03:26 +0700

    sasasa

---
+++ Pemrograman Berbasis Desktop/BelajarOOP/src/dasar/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dasar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ryori
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        /// Segitiga
+        // membuat objek dari class Segitiga
+        Matematika Segitiga = new Segitiga(4, 5);
+        // membuat objek dari class PersegiPanjang
+        Matematika PersegiPanjang = new PersegiPanjang(2, 12);
+        // menampikan output luas segitiga dan persegi panjang
+        System.out.println("Luas Segitiga: " + Segitiga.getArea());
+        System.out.println("Luas Persegi Panjang " + PersegiPanjang.getArea());
+
+    }
+}
